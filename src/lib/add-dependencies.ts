8f1de37106050525f349d8769536367b170f2183
Dharmen Shah <dharmen.s@milestoneinternet.com>
2021-02-27 16:55:11 +0530

    fix(add-dependencies.ts): add @ngneat/spectator to deps

--- src/lib/add-dependencies.ts
+++ src/lib/add-dependencies.ts
@@ -4,52 +4,51 @@ import { addPackageToPackageJsonFactory } from '../utils/package';
 import { Schema } from './schema';
 const { execSync } = require('child_process');
 
-const deps = [
-  {
-    name: '@commitlint/cli',
-    version: '^11.0.0',
-  },
-  {
-    name: '@commitlint/config-angular',
-    version: '^11.0.0',
-  },
-  {
-    name: '@commitlint/config-conventional',
-    version: '^11.0.0',
-  },
-  {
-    name: 'git-cz',
-    version: '^4.7.6',
-  },
-  {
-    name: 'all-contributors-cli',
-    version: '^6.19.0',
-  },
-  {
-    name: 'lint-staged',
-    version: '^10.5.3',
-  },
-  {
-    name: 'prettier',
-    version: '^2.2.1',
-  },
-  {
-    name: 'husky',
-    version: '^4.3.5',
-  },
-  {
-    name: 'cross-env',
-    version: '^7.0.3',
-  },
-];
-
 export function installDependencies(host: Tree, context: SchematicContext, options: Schema) {
-  const addPackageToPackageJson = addPackageToPackageJsonFactory(host, 'devDependencies');
+  const deps = [
+    {
+      name: '@commitlint/cli',
+      version: '^11.0.0',
+    },
+    {
+      name: '@commitlint/config-angular',
+      version: '^11.0.0',
+    },
+    {
+      name: '@commitlint/config-conventional',
+      version: '^11.0.0',
+    },
+    {
+      name: 'git-cz',
+      version: '^4.7.6',
+    },
+    {
+      name: 'all-contributors-cli',
+      version: '^6.19.0',
+    },
+    {
+      name: 'lint-staged',
+      version: '^10.5.3',
+    },
+    {
+      name: 'prettier',
+      version: '^2.2.1',
+    },
+    {
+      name: 'husky',
+      version: '^4.3.5',
+    },
+    {
+      name: 'cross-env',
+      version: '^7.0.3',
+    },
+  ];
+
   if (!options.skipLib) {
     if (!options.skipSpectator) {
       // install spectator synchronously so we can use it for external schematics command later on.
-      context.logger.log('info', '⚡ Executing- npm install --save-dev @ngneat/spectator');
       execSync('npm install --save-dev @ngneat/spectator');
+      deps.push({ name: '@ngneat/spectator', version: '^7.0.0' });
     }
 
     if (!options.skipAngularCliGhPages) {
@@ -58,6 +57,8 @@ export function installDependencies(host: Tree, context: SchematicContext, optio
       execSync('ng add angular-cli-ghpages');
     }
   }
+
+  const addPackageToPackageJson = addPackageToPackageJsonFactory(host, 'devDependencies');
 
   deps.forEach((dep) => {
     addPackageToPackageJson(dep.name, dep.version);
